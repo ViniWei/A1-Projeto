500fe04511cb53e4e29e2b5db0aa4c13d8d8e2a7
Vinícius Weigert <viniciusweigert152@gmail.com>
2024-06-17 23:25:31 -0300

    Project structure and test controller

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 import express, { Express, Request, Response } from "express";
-	
 import dotenv from "dotenv";
-	
- 
+import pingRoutes from './routes/pingRoutes'
+import swaggerUi from 'swagger-ui-express'
 	
 dotenv.config();
 	
- 
-	
 const app: Express = express();
-	
 const port = process.env.PORT || 4000;
 	
- 
-	
 app.get("/", (req: Request, res: Response) => {
-	
   res.send("Working!");
-	
 });
-	
- 
+
+app.use('/api/', pingRoutes);
+
+app.use(
+    "/swagger", /* endereço do swagger */
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+      swaggerOptions: {
+        url: "/swagger.json",
+      },
+    })
+);
 	
 app.listen(port, () => {
-	
   console.log(`[server]: Server is running at http://localhost:${port}`);
-	
 });
